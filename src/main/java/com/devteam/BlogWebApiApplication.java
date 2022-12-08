c69d0ac96bca9dc50bcd80239f41e6b83d748328
qnguyen95 <quangtri.nguyen@dxc.com>
2022-12-08 13:12:38 +0700

    Setup Project

---
+++ src/main/java/com/devteam/BlogWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.devteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWebApiApplication.class, args);
+	}
+
+}
